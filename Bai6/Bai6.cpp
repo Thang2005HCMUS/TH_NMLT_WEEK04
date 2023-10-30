bbd50c10985f433ff6f70347d69a50304b96a474
Thang2005HCMUS <D.Q.Thang.AI@gmail.com>
2023-10-30 09:51:48 +0700

    finish

--- Bai6/Bai6.cpp
+++ Bai6/Bai6.cpp
@@ -1,16 +1,36 @@
-#include <iostream>
+#include <ctime>   
+#include <iostream>  
+#include <Windows.h>
+#include<conio.h>
 using namespace std;
+
 int main() {
+	// current date/time based on current system
+	int second_user;
+	cout << "nhap so giay "; cin >> second_user;
+	cout << "nhap ESC de thoat " << endl;
+	for (int i = 1; i <= second_user; i++) {
 
+		time_t now = time(0);
+
+		tm* ltm = localtime(&now);
+
+		// print various components of tm structure.
+		cout << "Time: " << ltm->tm_hour << ":";
+		cout << ltm->tm_min << ":";
+		cout << ltm->tm_sec << endl;
+		if (_kbhit()) {
+			char COMMAND = _getch();
+			if (COMMAND == 27) break;
+
+		}if (ltm->tm_sec == 0) {
+			Beep(500, 500);
+			Sleep(500);
+		}
+		else {
+			Sleep(1000);
+		}
+
+
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
